d3a21b31360f6289e559219949777ed85328707a
العمدهه !! <qzmpty678@gmail.com>
2025-06-26 17:04:41 +0000

    Success. No rows returned

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Tajawal:wght@400;500;700&display=swap"
           rel="stylesheet"
         ></link>
       </head>
