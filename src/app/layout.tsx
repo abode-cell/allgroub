80243dd8e6e6b66fd0f6801488506701be0bce55
العمدهه !! <qzmpty678@gmail.com>
2025-07-02 02:50:04 +0000

    هناك تأخير عند بدء الموقع وتأخير عند التنقل بين الصفحات

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,13 @@ import { AuthProvider } from '@/contexts/auth-context';
 import { ClientLayout } from '@/components/client-layout';
 import { DataProvider } from '@/contexts/data-context';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
   title: 'منصة تمويل وإدارة استثمار متكاملة',
@@ -18,19 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        ></link>
-      </head>
-      <body className="font-body antialiased">
+      <head />
+      <body className={cn('font-sans antialiased', inter.variable)}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
