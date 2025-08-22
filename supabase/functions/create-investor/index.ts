08e78263d3ed7637103f3f0655c15c15a3a0131e
مجموعة عال <qzmpty678@gmail.com>
2025-08-22 09:38:13 +0000

    تمام قم بالتنفيذ

--- supabase/functions/create-investor/index.ts
+++ supabase/functions/create-investor/index.ts
@@ -1,4 +1,3 @@
-
 // supabase/functions/create-investor/index.ts
 
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
@@ -89,4 +88,7 @@ serve(async (req) => {
   } catch (error) {
     return new Response(JSON.stringify({ message: error.message }), {
       headers: { ...corsHeaders, "Content-Type": "application/json" },
-      status
+      status: 400,
+    });
+  }
+});
